559729b0e3dfad77be47c629ce6cbcdb7bf64ba7
shincap8 <shincap8@gmail.com>
2020-02-19 19:06:01 +0000

    test user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Test User"""
+import unittest
+import pep8
+from models.user import User
+
+
+class Testuser(unittest.TestCase):
+    """Test User"""
+
+    def test_pep8_conformance_user(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/user.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_User(self):
+        """
+        Test attributes of Class Use
+        """
+        my_user = User()
+        my_user.first_name = 'Holberton'
+        my_user.last_name = 'School'
+        my_user.email = '2020@holbertonschool.com'
+        self.assertEqual(my_user.first_name, 'Holberton')
+        self.assertEqual(my_user.last_name, 'School')
+        self.assertEqual(my_user.email, '2020@holbertonschool.com')
